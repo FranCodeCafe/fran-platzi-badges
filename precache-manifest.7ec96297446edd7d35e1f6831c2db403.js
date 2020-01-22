11e3db24821480f167a5f9cfc074a8c0c41f0ad9
FranVanHouten <franciscasalinascastillo@gmail.com>
2020-01-22 14:38:35 -0300

    Updates

---
+++ precache-manifest.7ec96297446edd7d35e1f6831c2db403.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/fran-platzi-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "bd8a93ea2fc5d2266d86",
+    "url": "/fran-platzi-badges/static/css/main.49892d76.chunk.css"
+  },
+  {
+    "revision": "ff3f45d454ba288351b0",
+    "url": "/fran-platzi-badges/static/js/runtime~main.ff3f45d4.js"
+  },
+  {
+    "revision": "fe6133392c61c74cf87b16047f88cc55",
+    "url": "/fran-platzi-badges/static/media/404-astronaut.fe613339.png"
+  },
+  {
+    "revision": "b46cf78879a898aae7cf",
+    "url": "/fran-platzi-badges/static/js/2.b46cf788.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/fran-platzi-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "bd8a93ea2fc5d2266d86",
+    "url": "/fran-platzi-badges/static/js/main.bd8a93ea.chunk.js"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/fran-platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/fran-platzi-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/fran-platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "400dc88500c0bc5e11f6c953dedcc7ab",
+    "url": "/fran-platzi-badges/static/media/twitter.400dc885.png"
+  },
+  {
+    "revision": "b46cf78879a898aae7cf",
+    "url": "/fran-platzi-badges/static/css/2.9808f87b.chunk.css"
+  },
+  {
+    "revision": "a066241e8184bc2e14d62d6661ed471a",
+    "url": "/fran-platzi-badges/index.html"
+  }
+];
